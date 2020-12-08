5bdbebf4ca363119fadff5e563f9e0c4f1ea213c
Praveen <apnerve@gmail.com>
2020-12-08 18:49:13 +0530

    chore: update to latest SDK version

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
 import '../styles/css/styles.scss';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://f5e95d3cdcba472e97f0cc1320387242@o488528.ingest.sentry.io/5549056",
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
+  
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
 
 ReactDOM.render(<App />, document.getElementById('app'));

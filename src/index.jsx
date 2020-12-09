3b006b8b3d72e814ff186d2291da3f3a7436cc71
Praveen <apnerve@gmail.com>
2020-12-09 11:24:22 +0530

    feat: teardown the meeting whenever HMSClient fires an 'disconnected' event

--- src/index.jsx
+++ src/index.jsx
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
 import '../styles/css/styles.scss';
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
 Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-      new Integrations.BrowserTracing(),
-    ],
-  
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
-  });
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById('app'));

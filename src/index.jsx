dac14a22c5da59b3cf66b9fdc626bbac0be84857
Praveen <praveen@100ms.live>
2020-12-23 16:26:40 +0530

    feat: integrate logrocket for frontend logs

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import 'antd/dist/antd.css';
 import '../styles/css/styles.scss';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
+import LogRocket from 'logrocket';
+LogRocket.init('plwzmx/100ms');
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -13,6 +15,12 @@ Sentry.init({
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
+});
+
+LogRocket.getSessionURL(sessionURL => {
+  Sentry.configureScope(scope => {
+    scope.setExtra('sessionURL', sessionURL);
+  });
 });
 
 ReactDOM.render(<App />, document.getElementById('app'));

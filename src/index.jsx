13dd40a2836a0c152d4d4e03ae6a0da42f2e4a26
Praveen <praveen@100ms.live>
2021-01-19 19:43:08 +0530

    chore: Remove sentry integration logRocket provides the same features

--- src/index.jsx
+++ src/index.jsx
@@ -3,26 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
 import '../styles/css/styles.scss';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import LogRocket from 'logrocket';
 if (process.env.NODE_ENV == 'production') {
   LogRocket.init('plwzmx/100ms');
 }
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [new Integrations.BrowserTracing()],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-});
-
-LogRocket.getSessionURL(sessionURL => {
-  Sentry.configureScope(scope => {
-    scope.setExtra('sessionURL', sessionURL);
-  });
-});
 
 ReactDOM.render(<App />, document.getElementById('app'));

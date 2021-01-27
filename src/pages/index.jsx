513d4696c29123ee05262c0c1357e323a012252c
Praveen <praveen@100ms.live>
2021-01-27 10:21:08 +0530

    chore: Remove logrocket integration for self-serve

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,4 @@
-import LogRocket from 'logrocket';
 import dynamic from 'next/dynamic';
-
-if (process.env.NODE_ENV == 'production') {
-  LogRocket.init('plwzmx/100ms');
-}
 
 const App = dynamic(import('../components/App'), { ssr: false });
 
